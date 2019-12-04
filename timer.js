2e3aff111de6bc333afa637a0ed7a86599212da5
Grace C <51942103+GraceeC@users.noreply.github.com>
2019-12-03 22:54:05 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,3 +1,37 @@
-let timer;
-let min = 0;
-let sec =0;
+function session(){
+let x = new Date();
+let min = x.getMinutes();
+let sec = x.getSeconds();
+let sessionBtn = document.getElementById('start');
+let sessionBtnReset = document.getElementById('reset');
+let startBrk = document.getElementById('startBrk');
+let resetBrake = document.getElementById('resetBrk');
+let minutesSeconds = `${min}:${sec}`;
+console.log(x);
+
+document.querySelector('.session').innerHTML = min + ':';
+document.querySelector('.session').innerHTML = sec;
+document.querySelector('.session').innerHTML = minutesSeconds;
+
+document.querySelector('.break').innerHTML = sec;
+document.querySelector('.break').innerHTML = sec;
+document.querySelector('.break').innerHTML = minutesSeconds;
+
+sessionBtn.addEventListener('click',function(e){
+console.log('start session btn clicked');
+});
+
+sessionBtnReset.addEventListener('click',function(e){
+console.log('reset session btn clicked');
+});
+startBrk.addEventListener('click',function(e){
+    console.log('start brake btn clicked');
+});
+resetBrake.addEventListener('click',function(e){
+    console.log('reset brake btn clicked');
+});
+
+}
+
+session();
+
