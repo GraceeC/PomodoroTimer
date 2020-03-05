b0ff082aacee11aae0af94ca010b52d4427f3000
Grace C <51942103+GraceeC@users.noreply.github.com>
2020-03-04 19:28:59 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -4,7 +4,8 @@ let brakeTimerTime = 300;
 
 // event listeners for Timer Butons & Brake buttons
 document.getElementById('start').addEventListener('click', () => {
-    longTimer();
+    console.log('start session timer clicked');
+   longTimer();
 });
 document.getElementById('pause').addEventListener('click', () => {
     console.log('paused clicked');
@@ -14,6 +15,7 @@ document.getElementById('reset').addEventListener('click', () => {
 });
 document.getElementById('startBrk').addEventListener('click', () => {
     console.log('start break btn clicked ');
+   
 });
 document.getElementById('pauseBrk').addEventListener('click', () => {
     console.log('pause brake btn clicked');
@@ -33,35 +35,33 @@ document.getElementById('resetBrk').addEventListener('click', () => {
 // });
 // }
 //toggle();
+function displayTimer() {
+    let minutes = parseInt(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+        document.querySelector('#sessionMinutes').innerHTML = minutes + ':' + seconds;
+    }
+}
+displayTimer();
 
 function longTimer() {
-            setInterval(() => {
-            totalSeconds = totalSeconds - 1;
-            let minutes = parseInt(totalSeconds / 60);
-            let seconds = totalSeconds % 60;
-            if (seconds < 10) {
-                seconds = '0' + seconds;
-            }
-            document.querySelector('#sessionMinutes').innerHTML =
-                minutes + ':' + seconds;
-            },1000);
+    setInterval(() => {
+        totalSeconds = totalSeconds - 1;
+        displayTimer();
+       
+ }, 1000);
 }
 
 
-longTimer();
+// function brakeTime() {
+//     let min = parseInt(brakeTimerTime / 60);
+//     console.log(min);
+//     let brakeSeconds = brakeTimerTime % 60;
+//     if (brakeSeconds < 10) {
+//         brakeSeconds = '0' + brakeSeconds;
+//     }
+//     document.querySelector('.text').innerHTML = min + ':' + brakeSeconds;
+// }
 
-function brakeTime() {
-    let brakeBtn = document.getElementById('startBrk');
-    brakeBtn.addEventListener('click', () => {
-        setInterval(() => {
-            brakeTimerTime = brakeTimerTime - 1;
-            let min = Math.round(brakeTimerTime / 60);
-            let seconds = Math.floor(brakeTimerTime % 60);
-            if (seconds < 10) {
-                seconds = '0' + seconds;
-            }
-            document.querySelector('.text').innerHTML = min + ':' + seconds;
-        }, 1000);
-    });
-}
-brakeTime();
+//brakeTime() 
