4f65c9abf0655e09b414a0316ddfc21d68961fbf
GraceeC <cardenasg85@yahoo.com>
2020-05-05 22:11:52 -0700

    simplified code

--- timer.js
+++ timer.js
@@ -1,154 +1,119 @@
-// // global variables
-// let totalSeconds = 1500;
-// let brakeTimerTime = 300;
-// let longTime;
-// let shortTime;
-// let sessionPause;
-// let pauseBrake;
-// let audio = new Audio('pinwheel.mp3')
-// console.log(audio);
+let workSeconds = 1500;
+let breakSeconds = 300;
+let workIntervalID;
+let brakeIntervalID;
 
-// // event listeners for Timer Butons & Brake buttons
-// document.getElementById("start").addEventListener("click", () => {
-//   console.log("start session timer clicked");
-//   longTimer();
-// });
-// document.getElementById("pause").addEventListener("click", () => {
-//   console.log("paused clicked");
-//   stopLongTimer();
-// });
-// document.getElementById("reset").addEventListener("click", () => {
-//   console.log("reset clicked");
-//   resetLongTimer();
-// });
-// document.getElementById("startBrk").addEventListener("click", () => {
-//   console.log("start break btn clicked ");
-//   shortTimer();
-// });
-// document.getElementById("pauseBrk").addEventListener("click", () => {
-//   console.log("pause brake btn clicked");
-//   stopBrakeTimer();
-// });
-// document.getElementById("resetBrk").addEventListener("click", () => {
-//   console.log("reset brake btn clicked");
-//   resetShortTimer();
-// });
-// //toggle event listener
-// document.getElementById("sliderBtn").addEventListener("click", (e) => {
-//   console.log('works');
-//   console.log('e.target.checked', e.target.checked);
-//   toggleBtn(e.target.checked);
-// });
+let audio = new Audio('pinwheel.mp3')
+console.log(audio);
 
-// // global variable for session timer
-// const longTimerDisplay = document.querySelector("#sessionMinutes");
+window.onload=function (e){
+displayWorkTimer(workSeconds, workTimerDisplay);
+displayBreakTimer(breakSeconds, brakeTimerDisplay);
+toggleBtn(false);
+}
 
-// //session timer function
-// function displayTimer(currentTime, display) {
-//   let minutes = parseInt(currentTime / 60);
-//   console.log(minutes);
-//   let seconds = currentTime % 60;
-//   console.log(seconds);
-//   if (seconds < 10) {
-//     seconds = "0" + seconds;
-//   }
-//   if (minutes < 10) {
-//     minutes = "0" + minutes;
-//   }
-//   if (currentTime < 1) {
-//     clearTimeout(longTime);
-//   }
-//   if (currentTime === 0) {
-//     audio.play();
-//   }
-//   longTimerDisplay.innerHTML = minutes + ":" + seconds;
-// }
-// //global variable for longTimer
-// displayTimer(totalSeconds, longTimerDisplay);
+document.getElementById('start').addEventListener("click",workTimer);
+document.getElementById('pause').addEventListener("click",stopWorkTimer);
+document.getElementById('reset').addEventListener("click",resetWorkTimer);
+document.getElementById('start-brk').addEventListener("click",shortTimer);
+document.getElementById('pause-brk').addEventListener("click",stopBreakTimer);
+document.getElementById('reset-brk').addEventListener("click",resetShortTimer);
 
-// //setInterval for my session timer
-// function longTimer() {
-//   longTime = setInterval(function () {
-//     displayTimer(totalSeconds, longTimerDisplay);
-//     totalSeconds = totalSeconds - 1;
-//   }, 1000);
-// }
+document.getElementById('slider-btn').addEventListener("click", (e) => {
+  toggleBtn(e.target.checked);
+});
 
-// //function to stop longTimer
-// function stopLongTimer() {
-//   clearTimeout(longTime);
-// }
+const workTimerDisplay = document.querySelector('#session-text');
 
-// function resetLongTimer() {
-//   stopLongTimer();
-//   let totalSeconds = 1500;
-//   displayTimer(totalSeconds, longTimerDisplay);
-// }
+function displayWorkTimer(currentTime, display) {
+  let minutes = parseInt(currentTime / 60);
+  console.log(minutes);
+  let seconds = currentTime % 60;
+  console.log(seconds);
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (currentTime < 1) {
+    clearTimeout(workTimeIntervalID);
+  }
+  if (currentTime === 0) {
+    audio.play();
+  }
+  workTimerDisplay.innerHTML = minutes + ":" + seconds;
+}
 
-// // //globarl variable for brake
-// const brakeTimerDisplay = document.querySelector(".text");
+function workTimer() {
+  workTimeIntervalID = setInterval(function () {
+    displayWorkTimer(workSeconds, workTimerDisplay);
+    workSeconds = workSeconds - 1;
+  }, 1000);
+}
 
-// //function for brake timer
-// function brakeTimer(brakeTime, displayBrake) {
-//   let min = parseInt(brakeTime / 60);
-//   console.log(min);
-//   let brakeSeconds = brakeTime % 60;
-//   console.log(brakeSeconds);
-//   if (brakeSeconds < 10) {
-//     brakeSeconds = "0" + brakeSeconds;
-//   }
-//   if (min < 10) {
-//     min = "0" + min;
+function stopWorkTimer() {
+  clearTimeout(workIntervalID);
+}
 
-//     // conditional used to restart timer once timer is complete
-//   }
-//   if (brakeTime < 1) {
-//     stopBrakeTimer();
-//     brakeTimerTime = 300;
-//     brakeTimer(brakeTimerTime, brakeTimerDisplay);
-//   }
-//   if (brakeTime === 0) {
-//     audio.play();
-//   }
-//   displayBrake.innerHTML = min + ":" + brakeSeconds;
-// }
-// brakeTimer(brakeTimerTime, brakeTimerDisplay);
+function resetWorkTimer() {
+  stopWorkTimer();
+  workSeconds = 1500;
+  displayWorkTimer(workSeconds, workTimerDisplay);
+}
 
-// //setInterval for brake timer
-// function shortTimer() {
-//   shortTime = setInterval(function () {
-//     brakeTimer(brakeTimerTime, brakeTimerDisplay);
-//     brakeTimerTime = brakeTimerTime - 1;
-//   }, 1000);
-// }
+const brakeTimerDisplay = document.querySelector('.text-break');
 
-// //function to stop shortTimer
-// function pauseShortTimer() {
-//   pauseBrake = setTimeout(() => {
-//     brakeTimer(brakeTimerTime, brakeTimerDisplay);
-//   }, 1000);
-// }
-// pauseShortTimer();
+function displayBreakTimer(brakeTime, displayBrake) {
+  let min = parseInt(brakeTime / 60);
+  console.log(min);
+  let breakSeconds = brakeTime % 60;
+  console.log(breakSeconds);
+  if (breakSeconds < 10) {
+    breakSeconds = "0" + breakSeconds;
+  }
+  if (min < 10) {
+    min = "0" + min;
 
-// //function to pause brake timer
-// function stopBrakeTimer() {
-//   clearTimeout(shortTime);
-// }
-// //funtion to reset brake timer
-// function resetShortTimer() {
-//   stopBrakeTimer();
-//   let brakeTimerTime = 300;
-//   brakeTimer(brakeTimerTime, brakeTimerDisplay);
-// }
-// //toggle Button between long timer & short timer
-// function toggleBtn(isBrakeTimer) {
-//   let shortTimeToggleBtn = document.getElementById('break');
-//   let longTimerSession = document.getElementById('sessionTime');
-//   if (isBrakeTimer === true) {
-//     longTimerSession.style.display = 'none';
-//     shortTimeToggleBtn.style.display = 'block';
-//   } else {
-//     longTimerSession.style.display = 'block';
-//     shortTimeToggleBtn.style.display = 'none';
-//   }
-// }
+    // conditional used to restart timer once timer is complete
+  }
+  if (brakeTime < 1) {
+    stopBrakeTimer();
+    breakSeconds = 300;
+    displayBreakTimer(breakSeconds, brakeTimerDisplay);
+  }
+  if (brakeTime === 0) {
+    audio.play();
+  }
+  displayBrake.innerHTML=min + ":" + breakSeconds;
+}
+
+function shortTimer() {
+  brakeIntervalID = setInterval(function () {
+    displayBreakTimer(breakSeconds, brakeTimerDisplay);
+    breakSeconds = breakSeconds - 1;
+  }, 1000);
+}
+
+function stopBrakeTimer() {
+  clearTimeout(breakIntervalID);
+}
+
+function resetShortTimer() {
+  stopBreakTimer();
+  breakSeconds = 300;
+  displayBreakTimer(breakSeconds, brakeTimerDisplay);
+}
+
+function toggleBtn(isBrakeTimer) {
+  let shortTimeToggleBtn = document.getElementById('container-three');
+  let workTimerSession = document.getElementById('session-time');
+  if (isBrakeTimer === true) {
+    resetWorkTimer();
+    workTimerSession.style.display='none'
+    shortTimeToggleBtn.style.display='block'
+  } else {
+    workTimerSession.style.display='block'
+    shortTimeToggleBtn.style.display='none';
+  }
+}
